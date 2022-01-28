9cdaedcd6b81bf66e64a3eac612d0624be5d3a60
janzhao <1258608694@qq.com>
2022-01-28 16:31:20 +0800

    leetcode

---
+++ offer45.java
@@ -0,0 +1,37 @@
+public class offer45 {
+    public static void main(String[] args) {
+        
+    }
+
+    public String minNumber(int[] nums) {
+        String[] strs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) strs[i] = String.valueOf(nums[i]);
+
+        //method1
+        quickSort(strs, 0, strs.length-1);
+
+        //method2
+        //Arrays.sort(strs, (x, y)->(x + y).compareTo(y + x));
+
+        StringBuffer res = new StringBuffer();
+        for (String s : strs) res.append(s);
+        return res.toString();
+    }
+    
+    public void quickSort(String[] strs, int l, int r) {
+        if (l >= r) return;
+        int i = l, j = r;
+        String tmp = strs[i];
+        while (i < j) {
+            while (i < j && (strs[j] + strs[l]).compareTo(strs[l] + strs[j]) >= 0) j--;
+            while (i < j && (strs[i] + strs[l]).compareTo(strs[l] + strs[i]) <= 0) i++;
+            tmp = strs[i];
+            strs[i] = strs[j];
+            strs[j] = tmp;
+        }
+        strs[i] = strs[l];
+        strs[l] = tmp;
+        quickSort(strs, l, i-1);
+        quickSort(strs, i+1, r);
+    }
+}
---
+++ offer61.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class offer61 {
+    public static void main(String[] args) {
+        int[] nums = {5,4,3,2,1};
+        offer61 isstra = new offer61();
+        System.out.println(isstra.isStraight(nums));
+    }
+
+    public boolean isStraight(int[] nums) {
+        Arrays.sort(nums);
+        int pre = nums[0], count = 0, i = 0;
+        while (i < 4) {
+            if (pre == 0) {
+                count++;
+                i++;
+                pre = nums[i];
+            } else {
+                if (pre + 1 == nums[i + 1]) {
+                    i++;
+                    pre = nums[i];
+                    continue;
+                }
+                if (count == 0) return false;
+                count--;
+                pre++;
+            }
+        }
+        return true;
+    }
+}
