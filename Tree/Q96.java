1b03e596a2fa6b8e52cc8d68ddf500009121c7d8
janzhao <1258608694@qq.com>
2022-03-14 11:49:49 +0800

    leetcode

---
+++ Tree/Q96.java
@@ -0,0 +1,29 @@
+package Tree;
+
+public class Q96 {
+    public static void main(String[] args) {
+        
+    }
+
+    public int numTrees(int n) {
+        int[] G = new int[n + 1];
+        G[0] = 1;
+        G[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
+    }
+
+    public int numTrees2(int n) {
+        // 提示：我们在这里需要用 long 类型防止计算过程中的溢出
+        long C = 1;
+        for (int i = 0; i < n; ++i) {
+            C = C * 2 * (2 * i + 1) / (i + 2);
+        }
+        return (int) C;
+    }
+}
